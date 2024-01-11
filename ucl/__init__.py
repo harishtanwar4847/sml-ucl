11717cfaacf7bd5b56c45eadb8f5f733ebb0a33c
Aditi Patade <aditi.patade@atriina.com>
2024-01-11 15:27:30 +0530

    Feat : Exceptions and Responder and supporting functions

--- ucl/__init__.py
+++ ucl/__init__.py
@@ -1,1 +1,125 @@
+import frappe
+import ucl
+
 __version__ = "0.0.1"
+
+
+user_token_expiry_map = {
+    "Login OTP": 10,
+    "Reset Pin OTP": 10,
+}
+
+def create_user_access_token(user_name):
+	user_details = frappe.get_doc('User', user_name)
+	api_secret = frappe.generate_hash(length=15)
+	# if api key is not set generate api key
+	if not user_details.api_key:
+		api_key = frappe.generate_hash(length=15)
+		user_details.api_key = api_key
+	user_details.api_secret = api_secret
+	user_details.save(ignore_permissions=True)
+
+	return 'token {}:{}'.format(user_details.api_key, api_secret)
+
+
+def create_user(first_name, last_name, mobile, email, tester):
+    try:
+        user = frappe.get_doc(
+            {
+                "doctype": "User",
+                "email": email,
+                "first_name": first_name,
+                "last_name": last_name,
+                "username": mobile,
+                "phone": mobile,
+                "mobile_no": mobile,
+                "send_welcome_email": 0,
+                "new_password": frappe.mock("password"),
+                "roles": [
+                    {"doctype": "Has Role", "role": "Loan Customer"},
+                    {"doctype": "Has Role", "role": "Spark Tester"},
+                ]
+                if tester
+                else [{"doctype": "Has Role", "role": "Loan Customer"}],
+            }
+        ).insert(ignore_permissions=True)
+
+        return user
+    except Exception as e:
+        raise ucl.exceptions.APIException(message=str(e))
+    
+
+def __user(input=None):
+    # get session user if input is not provided
+    if not input:
+        input = frappe.session.user
+    res = frappe.get_all("User", or_filters={"email": input, "username": input})
+
+    if len((res)) == 0:
+        raise ucl.exceptions.UserNotFoundException
+
+    return frappe.get_doc("User", res[0].name)
+    
+
+def token_mark_as_used(token):
+    if token.used == 0:
+        token.used = 1
+        token.save(ignore_permissions=True)
+        frappe.db.commit()
+
+def create_user_token(entity, token, token_type="OTP", app_version_platform=""):
+    doc_data = {
+        "doctype": "User Token",
+        "entity": entity,
+        "token": token,
+        "token_type": token_type,
+    }
+
+    expiry_in_minutes = user_token_expiry_map.get(token_type, None)
+    if expiry_in_minutes:
+        # expire previous OTPs
+        frappe.db.sql(
+            """
+			update `tabUser Token` set expiry = CURRENT_TIMESTAMP
+			where entity = '{entity}' and token_type = '{token_type}' and used = 0 and expiry > CURRENT_TIMESTAMP;
+		""".format(
+                entity=entity, token_type=token_type
+            )
+        )
+        doc_data["expiry"] = frappe.utils.now_datetime() + timedelta(
+            minutes=expiry_in_minutes
+        )
+
+    if app_version_platform:
+        doc_data["app_version_platform"] = app_version_platform
+        doc_data["customer_id"] = frappe.db.get_value(
+            "Loan Customer", {"user": entity}, "name"
+        )
+
+    user_token = frappe.get_doc(doc_data)
+    user_token.save(ignore_permissions=True)
+
+    return user_token
+
+
+def add_firebase_token(firebase_token, app_version_platform, user=None):
+    if not user:
+        user = frappe.session.user
+
+    
+    old_token = frappe.get_last_doc("User Token")
+    token_mark_as_used(old_token)
+
+    get_user_token = frappe.db.get_value(
+        "User Token",
+        {"token_type": "Firebase Token", "token": firebase_token, "entity": user},
+    )
+    if get_user_token:
+        return
+
+    create_user_token(
+        entity=user,
+        token=firebase_token,
+        token_type="Firebase Token",
+        app_version_platform=app_version_platform,
+    )
