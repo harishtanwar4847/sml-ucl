551e9701e16b40ce9114bb71e6223da26d752172
Aditi Patade <aditi.patade@atriina.com>
2024-02-26 14:52:58 +0530

    Feat : get partner list changes

--- ucl/used_car_loan/doctype/partner_kyc/partner_kyc.py
+++ ucl/used_car_loan/doctype/partner_kyc/partner_kyc.py
@@ -9,10 +9,12 @@ class PartnerKYC(Document):
     # pass
 	
 	def before_save(self):
-		if self.status in ["Rejected by SML", "Rejected by Partner"]:
-			partner_name = frappe.db.sql("select name from `tabPartner` where partner_kyc = '{name}'".format(name = self.name))
-			if partner_name:
-				partner = frappe.get_doc("Partner", partner_name[0][0])
+		partner_name = frappe.db.sql("select name from `tabPartner` where partner_kyc = '{name}'".format(name = self.name))
+		if partner_name:
+			partner = frappe.get_doc("Partner", partner_name[0][0])
+			if self.status in ["Rejected by SML", "Rejected by Partner"]:
 				partner.partner_kyc = ""
-				partner.save(ignore_permissions=True)
-				frappe.db.commit()
+			elif self.status== "Approved by SML":
+				partner.kyc_approved==1
+			partner.save(ignore_permissions=True)
+			frappe.db.commit()
